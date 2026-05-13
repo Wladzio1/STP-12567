cc5dee5357788422b72e796d29c44d74553da367
Wladek <vladislavfityak@gmail.com>
2026-05-13 16:52:31 +0200

    fixed the bgs in sections (fixes needed in Gallery)

--- src/js/swiper.js
+++ src/js/swiper.js
@@ -1,20 +1,21 @@
 const swiper = new Swiper('.gallery-swiper-main', {
   effect: 'coverflow',
   centeredSlides: true,
-  slidesPerView: 'auto', // Zostawiamy auto, ale CSS musi być mniejszy
-  initialSlide: 2, // Zaczynamy od środkowego (trzeciego) zdjęcia
+  slidesPerView: 'auto',
+  initialSlide: 2,
   loop: true,
+  autoHeight: false,
   coverflowEffect: {
     rotate: 0,
-    stretch: 220, // ZWIĘKSZ TO (np. z 80 na 120). Im więcej, tym ciasniej będą slajdy.
-    depth: 130, // Odpowiada za skalę bocznych slajdów.
+    stretch: 80,
+    depth: 200,
     modifier: 1,
     slideShadows: false,
   },
   breakpoints: {
     1440: {
       coverflowEffect: {
-        stretch: 120, // Większy ścisk na desktopie
+        stretch: 50,
       },
     },
   },
